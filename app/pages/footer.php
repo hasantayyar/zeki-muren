ff02ed0654c363d2513349d9efd0fa3c05a39e24
hasantayyar <tayyar.besik@gmail.com>
2014-09-08 12:01:20 +0300

    progressive caching

--- app/pages/footer.php
+++ app/pages/footer.php
@@ -1,58 +1,8 @@
-  </div>  
+</div>  
 
 <div class="footer"></div>
 
-<script src="//www.youtube.com/player_api"></script>
 <script>
-var player;
-var playButton = document.getElementById("play-button");
-var pauseButton = document.getElementById("pause-button");
-
-function showPlay(){
-  playButton.setAttribute("class",playButton.getAttribute("class").replace(/hide/g,""));
-  if ( !(" " + pauseButton.className + " ").replace(/[\n\t]/g, " ").indexOf("hide") > -1 ) {
-    pauseButton.setAttribute("class",pauseButton.getAttribute("class")+" hide");
-  }
-
-}
-function showPause(){
-  pauseButton.setAttribute("class",pauseButton.getAttribute("class").replace(/hide/g,""));
-  if ( !(" " + playButton.className + " ").replace(/[\n\t]/g, " ").indexOf("hide63") > -1 ) {
-    playButton.setAttribute("class",playButton.getAttribute("class")+" hide");
-  }
-}
-
-function loadVideo(id){
-  showPause();
-  play_link = document.getElementById(id);
-  $url = play_link.getAttribute("href");
-  console.log("Loading from youtube "+ $url);
-  player.loadVideoByUrl( $url );
-}
-
-// this function gets called when API is ready to use
-function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
-  player = new YT.Player('video', {
-    events: { 
-      'onReady': onPlayerReady
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  console.log("player loaded");
-  playButton.addEventListener("click", function() {
-    showPause();
-    player.playVideo();
-  });
-  pauseButton.addEventListener("click", function() {
-    showPlay();
-    player.pauseVideo();
-  });
-  
-}
-
 // analytics
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -61,12 +11,8 @@ function onPlayerReady(event) {
 
   ga('create', 'UA-1301980-42', 'auto');
   ga('send', 'pageview');
-
-
 </script>
-
 <div>
-
 </div>  
   </body>
 </html>
